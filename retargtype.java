94408905095ed1b68933d203416beba7f5776e72
rajasriganesan41-lang <rajasriganesan41@gmail.com>
2025-09-26 16:28:58 +0530

    Add files via upload

---
+++ retargtype.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+class rectangle{
+	public void sub(int x,int y){
+		
+		System.out.println("substraction="+(x-y));
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ public class retargtype{
+	 public static void main(String args[]){
+		 rectangle o=new rectangle();
+		 Scanner s=new Scanner(System.in);
+		 int a=s.nextInt();
+		 int b=s.nextInt();
+		 o.sub(a,b);
+	 }
+	 
+ }
+ 
